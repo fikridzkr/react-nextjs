1d1d863ee06ac834342ca64ca7f10a3e63e0e49d
fikridzkr <fikridzakir179@gmail.com>
2021-07-06 21:07:31 +0700

    view meetup and create form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image:
+      'https://www.expatica.com/app/uploads/sites/10/2014/05/best-place-to-live-in-uk.jpg',
+    address: 'BLAADADAD',
+    description: 'vddccdc',
+  },
+  {
+    id: 'm2',
+    title: 'Two Meetup',
+    image:
+      'https://images.movehub.com/wp-content/uploads/2017/09/14131021/Cambridge-2.jpg',
+    address: 'BLAADADAdsdsdD',
+    description: 'vddccdsdsdc',
+  },
+];
+
 function HomePage() {
-  return <h1>Home</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
