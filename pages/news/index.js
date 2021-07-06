30a854c8c6320d17d4f714e30440245c1a8fffd3
fikridzkr <fikridzakir179@gmail.com>
2021-07-06 20:30:05 +0700

    next router

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,19 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 function NewsPage() {
-    return <h1>News Page</h1>;
-  }
-  
-  export default NewsPage;
-  
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/fikri">Fikri</Link>
+        </li>
+        <li> Ronaldo</li>
+        <li>Pogba</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
