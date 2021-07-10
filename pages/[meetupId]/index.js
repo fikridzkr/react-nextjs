7f433637edbebc102c93ba9bf4989918959c2376
fikridzkr <fikridzakir179@gmail.com>
2021-07-10 11:25:36 +0700

    add custom components & css module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="dsdsd"
+      title="sdsdsd"
+      address="dsdsdsdsd"
+      description="dsddssd"
+    />
+  );
+}
