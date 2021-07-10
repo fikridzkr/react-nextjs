4aab0ed683e2008bfa620cc067135c7642972651
fikridzkr <fikridzakir179@gmail.com>
2021-07-10 14:29:20 +0700

    fix bug

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,11 +1,12 @@
-import {useRouter} from 'next/router'
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
-  const router = useRouter()
+  const router = useRouter();
   function showDetailsHandler() {
-    router.push('/' + props.id)
+    router.push('/' + props.id);
   }
   return (
     <li className={classes.item}>
